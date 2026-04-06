1df2a22fc9715e55e8f46290890559521b1f6786
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2026-04-05 20:16:01 -0500

    add INTorFLOAT, some work on postfix, and includes now work

--- lib/Log/Munger/RuleFileParser.pm
+++ lib/Log/Munger/RuleFileParser.pm
@@ -3,7 +3,11 @@ package Log::Munger::RuleFileParser;
 use 5.006;
 use strict;
 use warnings;
-use YAML::XS;
+use YAML::XS                   qw(Load);
+use Log::Munger::WhichRuleFile ();
+use File::Slurp                qw(read_file);
+use Template;
+use Hash::Merge ();
 
 =head1 NAME
 
@@ -58,124 +62,77 @@ sub load {
 
 	if ( !defined( $opts{'file'} ) ) {
 		die('$opts{file} is undef');
-	} elsif ( ref( $opts{'file'} ) eq '' ) {
+	} elsif ( ref( $opts{'file'} ) ne '' ) {
 		die( '$opts{file} ref is "' . ref( $opts{'file'} ) . '" and not ""' );
 	}
 
-	my $file_location = $self->rule_file_location( 'file' => $opts{'file'} );
-	if ( !defined($file_location) ) {
-		die( 'Unable to locate the location of the file "' . $opts{'file'} . '"' );
+	my $file_location = Log::Munger::WhichRuleFile->rule_file_location( 'file' => $opts{'file'} );
+
+	my $rules;
+	eval {
+		my $rules_yaml_raw = read_file($file_location);
+		$rules = Load($rules_yaml_raw);
+		if ( !defined($rules) ) {
+			die('Load(read_file($file_location)) returned undef');
+		}
+		if ( ref($rules) ne 'HASH' ) {
+			die( 'Load(read_file($file_location)) returned a ref of "' . ref($rules) . '" and not "HASH"' );
+		}
+	};
+	if ($@) {
+		die( 'Failed to read in and parse "' . $file_location . '"... ' . $@ );
 	}
 
-	
+	if ( defined( $rules->{'includes'} ) ) {
+		if ( ref( $rules->{'includes'} ) ne 'ARRAY' ) {
+			die(      '.includes in "'
+					. $file_location
+					. '" is of ref "'
+					. ref( $rules->{'incudes'} )
+					. '" and not "ARRAY"' );
+		}
+		if ( defined( $rules->{'includes'}[0] ) ) {
+			my $merger = Hash::Merge->new('RIGHT_PRECEDENT');
+			my %included;
+			my $include_int = 0;
+			my %left_side   = %{$rules};
+			while ( defined( $rules->{'includes'}[$include_int] ) ) {
+				if ( ref( $rules->{'includes'}[$include_int] ) ne '' ) {
+					die(      '.includes.'
+							. $include_int
+							. ' is a ref of "'
+							. ref( $rules->{'includes'}[$include_int] )
+							. '" and not ""' );
+				}
+
+				if ( !$included{ $rules->{'includes'}[$include_int] } ) {
+					$included{ $rules->{'includes'}[$include_int] } = 1;
+
+					my $include_location = Log::Munger::WhichRuleFile->rule_file_location(
+						'file' => $rules->{'includes'}[$include_int] );
+					my $rule_include;
+					eval {
+						my $raw_rules_include = read_file($include_location);
+						$rule_include = Load($raw_rules_include);
+						if ( !defined($rule_include) ) {
+							die( 'Load(read_file("' . $include_location . '")) returned undef for include' );
+						}
+						if ( ref($rule_include) ne 'HASH' ) {
+							die(      'Load(read_file("'
+									. $include_location
+									. '")) returned a ref of "'
+									. ref($rule_include)
+									. '" and not "HASH"' );
+						}
+					};
+
+					$rules = $merger->merge( \%left_side, $rule_include );
+				} ## end if ( !$included{ $rules->{'includes'}[$include_int...]})
+
+				$include_int++;
+			} ## end while ( defined( $rules->{'includes'}[$include_int...]))
+		} ## end if ( defined( $rules->{'includes'}[0] ) )
+	} ## end if ( defined( $rules->{'includes'} ) )
+
+	return $rules;
 } ## end sub load
-
-=head2 rule_file_location
-
-Returns the full path to the rule file specified.
-
-    - file :: The file to load.
-        Default :: undef
-
-=cut
-
-sub rule_file_location {
-	my ( $self, %opts ) = @_;
-
-	if ( !defined( $opts{'file'} ) ) {
-		die('$opts{file} is undef');
-	}
-
-	if ( $opts{'file'} =~ /^\// ) {
-		if ( -f $opts{'file'} ) {
-			return $opts{'file'};
-		} elsif ( -f $opts{'file'} . '.yaml' ) {
-			return $opts{'file'} . '.yaml';
-		} else {
-			return undef;
-		}
-	}
-
-	foreach my $rules_dir ( @{ $self->{'rules_dirs'} } ) {
-		if ( -d $rules_dir ) {
-			if ( -f $rules_dir . '/' . $opts{'file'} ) {
-				return $rules_dir . '/' . $opts{'file'};
-			} elsif ( -f $rules_dir . '/' . $opts{'file'} . '.yaml' ) {
-				return $rules_dir . '/' . $opts{'file'} . '.yaml';
-			}
-
-		}
-	} ## end foreach my $rules_dir ( @{ $self->{'rules_dirs'...}})
-
-	if ( $self->{'share_dir'} . '/' . $opts{'file'} ) {
-		return $self->{'share_dir'} . '/' . $opts{'file'};
-	} elsif ( $self->{'share_dir'} . '/' . $opts{'file'} . '.yaml' ) {
-		return $self->{'share_dir'} . '/' . $opts{'file'} . '.yaml';
-	}
-
-	return undef;
-} ## end sub rule_file_location
-
-=head2 process_item
-
-=cut
-
-sub process_item {
-	my ( $self, %opts ) = @_;
-
-}
-
-=head1 AUTHOR
-
-Zane C. Bowers-Hadley, C<< <vvelox at vvelox.net> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-log-munger at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Log-Munger>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Log::Munger
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Log-Munger>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/Log-Munger>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/Log-Munger>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-This software is Copyright (c) 2026 by Zane C. Bowers-Hadley.
-
-This is free software, licensed under:
-
-  The GNU General Public License, Version 3, June 2007
-
-
-=cut
-
-1;    # End of Log::Munger
